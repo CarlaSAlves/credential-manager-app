17d12ca17b797c28f602b3576db691223a744081
Carla A <carla.soeiro@gmail.com>
2025-06-14 21:44:06 +0100

    Implemented US2:  View Credentials List

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { Routes } from '@angular/router';
 import { RouterModule } from '@angular/router';
 // Update the path below to the correct location of HomeComponent, for example:
 import { HomeComponent } from './components/home/home.component';
+import { CredentialListComponent } from './components/credential-list/credential-list.component';
 
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    //{ path: 'credentials', component: CredentialsListComponent },
+    { path: 'credentials', component: CredentialListComponent },
     //{ path: 'add-credential', component: AddCredentialComponent },
 ];
 
