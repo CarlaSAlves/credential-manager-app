9bc37b45bf04a3f69de04cfbd41570d7be5375f0
Carla A <carla.soeiro@gmail.com>
2025-06-14 20:42:54 +0100

    Implenet US2: Added credetial-list-component

--- src/app/components/credential-list/credential-list.component.ts
+++ src/app/components/credential-list/credential-list.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CredentialService } from '../../services/credential.service';
 
 @Component({
   selector: 'app-credential-list',
-  imports: [],
   templateUrl: './credential-list.component.html',
-  styleUrl: './credential-list.component.scss'
+  styleUrls: ['./credential-list.component.scss']  
 })
 export class CredentialListComponent {
+  credentials: Credential[] = [];
 
+  constructor(private credentialService: CredentialService) { }
+
+  ngOnInit() {
+    this.loadCredentials();
+  }
+  
+  /**get credentials to the service */
+  loadCredentials(): void {
+    this.credentialService.getCredentials();
+  }
+
+      /**Remove credentials by id */
+  removeCredential(id: string): void {
+      if(confirm('Are you sure you want to delete this credential?')) {
+          // Call the service to remove the credential    
+        this.credentialService.removeCredential(id);
+         // Reload the credentials after deletion
+        this.loadCredentials();
+        }   
+      }
 }
