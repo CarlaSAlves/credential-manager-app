dff9f7d0d7ed786e1805e2230861ac5746d25eac
Carla A <carla.soeiro@gmail.com>
2025-06-14 14:32:04 +0100

    us1 - routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
+import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
+// Update the path below to the correct location of HomeComponent, for example:
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    //{ path: 'credentials', component: CredentialsListComponent },
+    //{ path: 'add-credential', component: AddCredentialComponent },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
